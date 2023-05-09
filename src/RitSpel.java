db898a4b3b191bb00635dc84657b9144abceaf15
Nn0el <noel.johansson@elev.ga.ntig.se>
2023-05-09 15:04:39 +0200

    Skapat canvas och mer

--- src/RitSpel.java
+++ src/RitSpel.java
@@ -6,14 +6,17 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
-public class RitSpel extends Canvas {
+public class RitSpel extends Canvas implements Runnable{
     private BufferStrategy bs;
     private boolean running = false;
     private Thread thread;
 
+    private BufferedImage image;
     public RitSpel() {
         setSize(600, 400);
+        image = new BufferedImage(600,400,BufferedImage.TYPE_INT_ARGB);
         JFrame frame = new JFrame();
         frame.add(this);
         this.addMouseMotionListener(new MyMouseMotionListener());
@@ -34,7 +37,11 @@ public class RitSpel extends Canvas {
 
         @Override
         public void mouseDragged(MouseEvent e) {
-
+            int mouseX = e.getX();
+            int mouseY = e.getY();
+            Graphics g = image.getGraphics();
+            g.setColor(new Color(0xFF0000));
+            g.fillOval(mouseX, mouseY,10,10);
         }
     }
 
@@ -43,7 +50,9 @@ public class RitSpel extends Canvas {
         public void mouseClicked(MouseEvent e) {
             int mouseX = e.getX();
             int mouseY = e.getY();
-            DrawRect(mouseX, mouseY);
+            Graphics g = image.getGraphics();
+            g.setColor(new Color(0xFF0000));
+            g.fillOval(mouseX, mouseY,10,10);
         }
 
         @Override
@@ -69,7 +78,8 @@ public class RitSpel extends Canvas {
     }
 
     public static void main(String[] args) {
-
+        RitSpel noelsSpel = new RitSpel();
+        noelsSpel.start();
     }
 
     public synchronized void stop() {
@@ -80,4 +90,57 @@ public class RitSpel extends Canvas {
             e.printStackTrace();
         }
     }
+
+    public void render() {
+        bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+        // Rita ut den nya bilden
+        draw(g);
+
+        g.dispose();
+        bs.show();
+    }
+
+    public void draw(Graphics g) {
+        g.clearRect(0,0,getWidth(),getHeight());
+        g.drawImage(image,0,0,null);
+    }
+
+    private void update() {
+    }
+
+
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void run() {
+        double ns = 1000000000.0 / 60.0;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+
+            while(delta >= 1) {
+                // Uppdatera koordinaterna
+                update();
+                // Rita ut bilden med updaterad data
+                render();
+                delta--;
+            }
+        }
+        stop();
+    }
+
+
 }
